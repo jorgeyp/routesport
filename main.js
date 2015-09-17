f951bff5f15c8f5e383d6b1045246d6faeaed34d
Jorge Yagüe París <jorgeyp@gmail.com>
2015-09-17 20:16:07 +0200

    Fix overlay not showing up. :hammer:

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 var centre = new google.maps.LatLng(43.5314284, -5.6684546);
 var map;
-
 var wmsUrl = "http://ide.gijon.es:8080/geoserver/wms?SERVICE=WMS&VERSION=1.1.1";
 var wmsLayers = "Gijon:LU_Zona_Verde,Gijon:Rutas_Verdes";
-var wmsStyle = "polygon,line"
+//var wmsStyle = "polygon,line"
 
 var TileWMS = function(coord,zoom) {
-  var x = document.getElementById("wms-texto");
   var proj = map.getProjection(); 
   var zfactor = Math.pow(2, zoom); 
   var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor) ); 
   var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor)); 
   var bbox = top.lng() + "," + bot.lat() + "," + bot.lng() + "," + top.lat();
       
-  var wmsMapUrl = wmsUrl + "&REQUEST=GetMap&CRS=EPSG:4326&WIDTH=256&HEIGHT=256&FORMAT=image/png&TRANSPARENT=true&LAYERS=" + wmsLayers + "&STYLE=" + wmsStyle + "&BBOX=" + bbox;
-  
-  console.log(wmsMapUrl)
+  var wmsParameters = wmsUrl + "&REQUEST=GetMap&CRS=EPSG:4326&WIDTH=256&HEIGHT=256&FORMAT=image/png&TRANSPARENT=true&LAYERS=" + wmsLayers 
+//  + "&STYLE=" + wmsStyle 
+  + "&BBOX=" + bbox;
 
-  return wmsUrl;
+  return wmsParameters;
 }
-
 
 function initialize() {
   console.log("Init")
