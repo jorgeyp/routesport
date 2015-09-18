9311fcd7a01bbddb9111ba79ea787f6c779cb87f
Jorge Yagüe París <jorgeyp@gmail.com>
2015-09-18 11:24:12 +0200

    Add click events

--- main.js
+++ main.js
@@ -3,19 +3,48 @@ var map;
 var wmsUrl = "http://ide.gijon.es:8080/geoserver/wms?SERVICE=WMS&VERSION=1.1.1";
 var wmsLayers = "Gijon:LU_Zona_Verde,Gijon:Rutas_Verdes,Gijon:Golf,Gijon:Instalaciones_Deportivas,Gijon:Playas";
 //var wmsStyle = "polygon,line"
+var marker;
+var TILE_SIZE = 256;
 
 var TileWMS = function(coord,zoom) {
   var proj = map.getProjection(); 
   var zfactor = Math.pow(2, zoom); 
-  var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * 256 / zfactor, coord.y * 256 / zfactor) ); 
-  var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * 256 / zfactor, (coord.y + 1) * 256 / zfactor)); 
+  var top = proj.fromPointToLatLng(new google.maps.Point(coord.x * TILE_SIZE / zfactor, coord.y * TILE_SIZE / zfactor) ); 
+  var bot = proj.fromPointToLatLng(new google.maps.Point((coord.x + 1) * TILE_SIZE / zfactor, (coord.y + 1) * TILE_SIZE / zfactor)); 
   var bbox = top.lng() + "," + bot.lat() + "," + bot.lng() + "," + top.lat();
       
   var wmsParameters = wmsUrl + "&REQUEST=GetMap&CRS=EPSG:4326&WIDTH=256&HEIGHT=256&FORMAT=image/png&TRANSPARENT=true&LAYERS=" + wmsLayers 
 //  + "&STYLE=" + wmsStyle 
   + "&BBOX=" + bbox;
 
+  console.log(wmsParameters)
+  
   return wmsParameters;
+}
+
+function addMarker(location) {
+  if(marker != null)  
+    marker.setMap(null);
+  marker = new google.maps.Marker({
+    position: location,
+    map: map
+    //icon:
+  });
+}
+
+// The mapping between latitude, longitude and pixels is defined by the web
+// mercator projection.
+// From: https://developers.google.com/maps/documentation/javascript/examples/map-coordinates
+function project(latLng) {
+  var siny = Math.sin(latLng.lat() * Math.PI / 180);
+
+  // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+  // about a third of a tile past the edge of the world tile.
+  siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+
+  return new google.maps.Point(
+      TILE_SIZE * (0.5 + latLng.lng() / 360),
+      TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)));
 }
 
 function initialize() {
@@ -37,10 +66,26 @@ function initialize() {
   
   var overlayOptions = {
     getTileUrl: TileWMS,
-    tileSize: new google.maps.Size(256, 256),
+    tileSize: new google.maps.Size(TILE_SIZE, TILE_SIZE),
     opacity: 0.35
   };
   var overlayWMS = new google.maps.ImageMapType(overlayOptions);
   map.overlayMapTypes.push(overlayWMS);
+  
+  google.maps.event.addListener(map, 'click', function(event) {
+    var scale = 1 << map.getZoom();
+    var worldCoordinate = project(event.latLng);
+    var pixelCoordinate = new google.maps.Point(
+        Math.floor(worldCoordinate.x * scale),
+        Math.floor(worldCoordinate.y * scale));
+    var tileCoordinate = new google.maps.Point(
+      Math.floor(worldCoordinate.x * scale / TILE_SIZE),
+      Math.floor(worldCoordinate.y * scale / TILE_SIZE));
+    
+//    var projection = map.getProjection();
+//    var pixel = projection.fromLatLngToContainerPixel(event.latLng);
+    console.log(tileCoordinate);
+    addMarker(event.latLng);
+  });
   
 }
